9372ac9cd630d9e46b512643d26b6b4a61c5aa5b
Yamini Ulaganathan <uyamini1996@gmail.com>
2022-05-16 22:32:52 -0500

    it's taking forever to run tho

--- interpreter/awards_temp.py
+++ interpreter/awards_temp.py
@@ -14,79 +14,40 @@ spacy.load('en_core_web_md')
 nltk.download('punkt')
 
 
-def find_nominees(data, awards, year):
+def find_awards(data, awards, year):
 
     nominees = {award: [] for award in awards}
     # Track number of times each potential nominee is mentioned for each award
-    nominee_tracker = {award: Counter() for award in awards}
-
     filtered_data = filter_data(data)
 
     functs_to_map = [utilities.remove_punctuation,
                      utilities.lower, 
                      utilities.remove_stop_words, 
                      utilities.stem]
-    preprocessed_awards = preprocess_awards_for_similarity(awards, functs_to_map)
+    preprocessed_tweets = preprocess_tweets(filtered_data, functs_to_map)
 
    # ia = imdb.IMDb()
 
     # Load NER tagger
     nlp = en_core_web_md.load()
 
-    allowed_labels = {'PERSON', 'WORK_OF_ART', 'EVENT', 'ORGANIZATIONS', 'PRODUCT','LANGUAGE'}
+  #  allowed_labels = {'PERSON', 'WORK_OF_ART', 'EVENT', 'ORGANIZATIONS', 'PRODUCT','LANGUAGE'}
+    allowed_labels = {'PERSON', 'WORK_OF_ART', 'EVENT', 'ORGANIZATIONS', 'NORP', 'GPE', 'LOCATION', 'PRODUCT'}
 
-    for award in preprocessed_awards:
-        matches = [match[0] for match in extractBests(award, text_lst, limit=50)]
-        for nominee in nominees[award]:
-            candidates = [candidate[0] for candidate in extractBests(nominee, matches, limit=25)]
+    res = []
+    for tweet in preprocessed_tweets:
         
-        tagged_text = list(map(nlp, candidates))
-        likely_winners = [chunk.text for seq in tagged_text for chunk in seq.ents if chunk.label_ in allowed_labels ]
-        counter = Counter(likely_winners)
+        tagged_text = list(map(nlp, tweet))
+        likely_award_chunks = [chunk.text for seq in tagged_text for chunk in seq.ents if chunk.label_ in allowed_labels ]
+        counter = Counter(filter(bool, likely_award_chunks))
+        if len(counter): res.extend([ item[0] for item in counter.most_common(1) ])
 
-        # Keep logic in case of arbitrary selection, multiple names
-        winner[award] = [item[0] for item in counter.most_common(1)]
-
-    return winner
-
-    for post in filtered_data:
-
-        # Tag post with named entities
-        ner_tagged_post = nlp(post)
-
-        # Get all named entity chunks
-        likely_nominees = [chunk.text.lower() for chunk in ner_tagged_post.ents]
-
-        for nominee in likely_nominees:
-            preprocessed_post = preprocess_post_for_similarity(post, functs_to_map)
-
-            # Assign post to most likely award or None if none likely
-            award = get_relevant_award(preprocessed_post, preprocessed_awards)
-
-            # Increment nominee counter
-            if award:
-                nominee_tracker[award][nominee] += 1
-
-    for award, nominee_tracker in nominee_tracker.items():
-        for likely_nominee in nominee_tracker.keys():
-            if len(nominees[award]) == 5: break
-            if any(token in award for token in ['actor', 'actress', 'director', 'producer', 'writer']):
-                closest = imdb_get_similar(likely_nominee, ia, year, 'person')
-            elif any(token in award for token in ['series', 'television']):
-                closest = imdb_get_similar(likely_nominee, ia, year, 'tv series')
-            else:
-                closest = imdb_get_similar(likely_nominee, ia, year, 'movie')
-            if closest and edit_distance(likely_nominee, closest) < 3:
-                if closest not in nominees[award]:
-                    nominees[award].append(closest)
-                print(f'{closest} added to {award}')
-
-    return nominees
+    return res
 
 
 def filter_data(data):
     text_lst = [post['text'] for post in data]
-    with open('./interpreter/patterns/nominee_patterns.json', 'r') as f:
+    with open('./interpreter/patterns/awards_patterns.json', 'r') as f:
         patterns = json.load(f)
     text_lst = [re.sub('|'.join(patterns['remove_patterns']), '', text, flags=re.IGNORECASE) for text in text_lst]
     # Filter posts that contain none of the negative match tokens and any of the positive match tokens
@@ -96,7 +57,7 @@ def filter_data(data):
 
 
 def preprocess_awards_for_similarity(awards, functs_to_map):
-    # mod_awards = [award.replace('best', '').replace('award', '').strip() for award in awards]
+    mod_awards = [award.replace('best', '').replace('award', '').strip() for award in awards]
     for func in functs_to_map:
         mod_awards = map(func, mod_awards)
     return dict(zip(awards, list(mod_awards)))
@@ -123,3 +84,16 @@ def preprocess_tweets(tweets, funcs):
     for func in funcs:
         tweets = map(func, tweets)
     return list(tweets)
+
+
+
+
+
+
+
+
+# s = 'Timothee Chalamet totally deserved to win Best Actor! He is the loml.'
+# nlp = en_core_web_md.load()
+
+# for chunk in nlp(s).ents:
+#     print(chunk.text, chunk.label_)
--- interpreter/gg_api.py
+++ interpreter/gg_api.py
@@ -5,6 +5,8 @@ import os.path
 import host_interpreter as host
 import nominee_interpreter as nominee
 import presenter_interpreter as presenter
+import awards_temp as awardnames
+
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
@@ -30,6 +32,8 @@ def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
+    loaded_data = fetch_data(year)
+    awards = awardnames.find_awards(loaded_data, OFFICIAL_AWARDS_1315, year)
     return awards
     
 def get_nominees(year):
@@ -76,9 +80,10 @@ def main():
     start = time.time()
     years = ['2013', '2015']
     for year in years:
-        print(f'{year} hosts: {get_hosts(year)}')
-        print(f'{year} nominees: {get_nominees(year)}')
-        print(f'{year} presenters: {get_presenters(year)}')
+        # print(f'{year} hosts: {get_hosts(year)}')
+        # print(f'{year} nominees: {get_nominees(year)}')
+        # print(f'{year} presenters: {get_presenters(year)}')
+        print(f'{year} award names: {get_awards(year)}')
     finish = time.time()
     #print elapsed time in minutes and seconds
     print(f'Elapsed time: {(finish - start) / 60} minutes')
